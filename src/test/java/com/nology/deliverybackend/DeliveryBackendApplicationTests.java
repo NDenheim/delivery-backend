23bb526b4c6fe3e83937badc7f51c805ade7b8c4
NDenheim <nicole.denheim@hotmail.co.uk>
2023-09-28 16:43:09 +0100

    Initial commit

---
+++ src/test/java/com/nology/deliverybackend/DeliveryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nology.deliverybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
